0c1f00d5fbc0fb4fae0856ec8c650406ae975f58
Tarik Mey <tarik.mey@gmail.com>
2023-06-24 09:20:44 +0200

    adding canvas settings

---
+++ project.js
@@ -0,0 +1,19 @@
+// Canvas settings
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+// const window_height = window.innerHeight;
+// const window_width = window_height;
+
+// canvas.width = window_width;
+// canvas.width = window_height;
+canvas.width = 300;
+canvas.height = 800;
+
+canvas.style.background = "red";
+
+// background: #1D1E18
+// 1. #6B8F71
+// 2. #AAD2BA
+// 3. #D9FFF5
+// 4. #B9F5D8
